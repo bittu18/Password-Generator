4fe5afa06697b3cca7ff94f90aa2e4bd979c9ac8
Bittu Bhardwaj <40118751+bittu18@users.noreply.github.com>
2018-11-16 12:02:54 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+"""
+to choose random char you'll need import the random module
+"""
+import random
+"""
+now u will choose a random char from the list and store it in a variable called
+password"""
+chars= 'abcdefghijklmnopqrstuvwxyzABCDEFGHI1234567890'
+# enter the size of password as urs requriment
+length=input('Enter the size of password:')
+#use int() to turn the user's input into a whole number
+length=int(length)
+# add this code to create 3 passwords
+for p in range(3):
+    password=''
+    for c in range(length):
+        password +=random.choice(chars)
+    print(password)
+
